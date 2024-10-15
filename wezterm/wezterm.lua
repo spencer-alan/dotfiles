0e0e8b581903718cc6609d8526c4aaac90c8a271
Spencer Smith <smith.spence13@gmail.com>
2024-10-15 08:48:14 -0700

    Wezterm added w/ other config updates to support

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Tokyo Night Storm"
+config.force_reverse_video_cursor = true
+
+config.font = wezterm.font("DankMono Nerd Font")
+config.font_size = 19
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+return config
