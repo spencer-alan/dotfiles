fe3d1bff6fd6b944545d2ef52405231fb45a6328
Spencer Smith <smith.spence13@gmail.com>
2024-10-28 21:37:45 -0700

    Updated autopairs for nvim; starship for prompt

--- nvim/lua/smith/plugins/colorscheme.lua
+++ nvim/lua/smith/plugins/colorscheme.lua
@@ -15,10 +15,23 @@ return {
 --       dark_theme = true, -- Set the theme with dark background.
 --       transparent = false, -- Set transparent background.
 --       high_contrast = false, -- Make the dark background darker and the light background lighter.
---       fluo_color = 'pink', --  Fluo color: pink, yellow, orange, or green.
+--       fluo_color = 'green', --  Fluo color: pink, yellow, orange, or green.
 --       mode = 'normal', -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
 --       aggressive_spell = false, -- Display colors for spell check.
 --     }
 --     vim.cmd 'colorscheme flow'
+--   end,
+-- }
+-- return {
+--   'neanias/everforest-nvim',
+--   version = false,
+--   lazy = false,
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   -- Optional; default configuration will be used if setup isn't called.
+--   opts = {},
+--   config = function()
+--     require('everforest').setup {
+--       -- Your config here
+--     }
 --   end,
 -- }
